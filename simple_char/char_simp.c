64dcc6195be80b8e5ae12f86a78738fae4008e3d
Siddharth Menon <simeddon@gmail.com>
2024-05-14 21:37:45 +0530

    enter size of array as parameter

--- simple_char/char_simp.c
+++ simple_char/char_simp.c
@@ -4,6 +4,7 @@
 #include <linux/cdev.h>
 #include <linux/types.h> //dev_t
 #include <linux/uaccess.h> //access userspace
+#include <linux/moduleparam.h> //access userspace
 
 #define DEVICE_NAME "char_simp_dev"
 
@@ -13,19 +14,21 @@ MODULE_DESCRIPTION("Simple character driver");
 
 static int major_num;
 static struct cdev le_cdev;
-int size;
+static int size = 255;
 char *charArray;
 static int buffer_ptr = 0;
 
+module_param(size, int, 0644);
+
 static int simple_char_open(struct inode *inode, struct file *instance) {
-    printk(KERN_INFO "char_simp_dev: open()\n");
+    printk(KERN_INFO "char_simp_dev: opened device\n");
     return 0;
 }
 
-ssize_t read(struct file *FILE, char *user_buffer, size_t count, loff_t *offset) {
+ssize_t dev_read(struct file *FILE, char *user_buffer, size_t count, loff_t *offset) {
     int to_cpy, not_cpy, delta;
 
-    to_cpy = min(count, buffer_ptr - *offset);
+    to_cpy = min_t(int, count, buffer_ptr - *offset);
     not_cpy = copy_to_user(user_buffer, charArray + *offset, to_cpy);
 
     if (not_cpy != 0) {
@@ -40,11 +43,12 @@ ssize_t read(struct file *FILE, char *user_buffer, size_t count, loff_t *offset)
     return delta;
 }
 
-ssize_t write (struct file *File, const char *user_buffer, size_t count, loff_t *offset){
+ssize_t dev_write (struct file *File, const char *user_buffer, size_t count, loff_t *offset){
     buffer_ptr = 0;
     int to_cpy, not_cpy, delta;
 
-    to_cpy = min(count, sizeof(charArray) - buffer_ptr);
+    to_cpy = min_t(int, count, size - buffer_ptr);
+    printk(KERN_INFO "char_simp_dev: count: %d length of charArray: %d buffer_ptr: %d bytes to copy: %d", count, size, buffer_ptr, to_cpy);
 
     not_cpy = copy_from_user(charArray + buffer_ptr, user_buffer, to_cpy);
     
@@ -56,19 +60,19 @@ ssize_t write (struct file *File, const char *user_buffer, size_t count, loff_t 
     delta = to_cpy - not_cpy;
 
     printk(KERN_INFO "char_simp_dev: written ");
-
-    for (int i = 0; i<buffer_ptr; i++) {
+    
+    /*for (int i = 0; i<buffer_ptr; i++) {  //for debugging
         printk(KERN_INFO "%c", charArray[i]);
-    };
+    };*/
     
     printk(KERN_INFO "char_simp_dev: written %d bytes\n", to_cpy);
     return delta;
 }
 
 static int simple_char_release(struct inode *inode, struct file *instance) {
-    for (int i = 0; i<buffer_ptr; i++) {
+    /*for (int i = 0; i<buffer_ptr; i++) {  //for debugging
         printk(KERN_INFO "%c", charArray[i]);
-    };
+    };*/
     printk(KERN_INFO "char_simp_dev: driver closed\n");
     return 0;
 }
@@ -77,16 +81,15 @@ static const struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = simple_char_open,
     .release = simple_char_release,
-    .read = read,
-    .write = write,
+    .read = dev_read,
+    .write = dev_write,
 };
 
 static int __init simple_char_init(void) {
     dev_t dev_num;
     int regval;
 
-    size = 255;
-
+    printk(KERN_INFO "char_simp_dev: Buffer size: %d\n", size);
     charArray = kmalloc(size * sizeof(char), GFP_KERNEL);
     if (charArray == NULL) {
         return -ENOMEM;
