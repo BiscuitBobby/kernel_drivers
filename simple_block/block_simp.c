3adb363b8001811ccc7b18a07050264b4781a9cd
Siddharth Menon <simeddon@gmail.com>
2025-02-25 21:01:48 +0530

    allocate disk

--- simple_block/block_simp.c
+++ simple_block/block_simp.c
@@ -3,12 +3,41 @@
 #include <linux/blkdev.h>
 #include <linux/fs.h>
 
+#define MY_BLOCK_MINORS 1
 #define MY_BLOCK_MAJOR 240
 #define MY_BLKDEV_NAME "block_simp_dev"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BiscuitBobby");
 MODULE_DESCRIPTION("Simple Block Device Driver");
+
+static struct my_block_dev {
+    struct gendisk *gd;
+} dev;
+
+static int create_block_device(struct my_block_dev *dev)
+{
+    int ret;
+    dev->gd = blk_alloc_disk(NULL, NUMA_NO_NODE);
+    if (!dev->gd) {
+        printk(KERN_ERR "block_simp_dev: Failed to allocate gendisk\n");
+        return -ENOMEM;
+    }
+
+    ret = add_disk(dev->gd);
+    if (ret) {
+        printk(KERN_ERR "block_simp_dev: Failed to add disk\n");
+        return ret;
+    }
+
+    return 0;
+}
+
+static void delete_block_device(struct my_block_dev *dev)
+{
+    if (dev->gd)
+        del_gendisk(dev->gd);
+}
 
 static int my_block_init(void)
 {
@@ -20,13 +49,19 @@ static int my_block_init(void)
         return reg_stat;
     }
 
+    if (create_block_device(&dev) < 0) {
+        unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
+        return -ENOMEM;
+    }
+
     printk(KERN_INFO "block_simp_dev: Block device registered\n");
     return 0;
 }
 
 static void my_block_exit(void)
 {
-    unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);  //returns void value
+    delete_block_device(&dev);
+    unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
     printk(KERN_INFO "block_simp_dev: Block device unregistered\n");
 }
 
